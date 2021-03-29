ad002167fb8b96cfbbd98598ca7a48cec62bdb58
mrmokwa <matheus.mokwa@gmail.com>
2021-03-29 10:36:11 -0300

    Ajustar ordenacao

--- src/app/pedvend/components/index.ts
+++ src/app/pedvend/components/index.ts
@@ -1,5 +1,5 @@
 export * from './pedvend-datagrid/pedvend-datagrid.component';
 export * from './pedvend-datagrid-detail/pedvend-datagrid-detail.component';
+export * from './pedvend-datagrid-filter-situacao/pedvend-datagrid-filter-situacao.component';
 export * from './pedvend-detalhes/pedvend-detalhes.component';
 export * from './pedvend-itens/pedvend-itens.component';
-export * from './pedvend-datagrid-filter-situacao/pedvend-datagrid-filter-situacao.component';
