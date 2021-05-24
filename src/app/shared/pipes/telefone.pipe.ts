89c0c9fa21342759f739ec86f0f728f1eae3bfa4
mrmokwa <matheus.mokwa@gmail.com>
2021-05-24 15:28:50 -0300

    ajustar exibicao de telefones

--- src/app/shared/pipes/telefone.pipe.ts
+++ src/app/shared/pipes/telefone.pipe.ts
@@ -3,8 +3,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 const maskPhone = (value: string) => {
   return new String(value)
     .replace(/\D/g, '')
-    .replace(/(\d{2})(\d)/, '($1) $2')
-    .replace(/(\d{5})(\d{4})(\d)/, '$1-$2');
+    .replace(/^(\d{2})(\d)/g, '($1) $2')
+    .replace(/(\d)(\d{4})$/, '$1-$2');
 };
 
 @Pipe({
