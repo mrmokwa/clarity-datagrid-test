918e3be021432106fb1b85bb7a63b281819f63f6
mrmokwa <matheus.mokwa@gmail.com>
2021-05-25 10:31:17 -0300

    tweak filter focus

--- src/app/cliente/components/cliente-datagrid-situacao-filter/cliente-datagrid-situacao-filter.component.ts
+++ src/app/cliente/components/cliente-datagrid-situacao-filter/cliente-datagrid-situacao-filter.component.ts
@@ -2,9 +2,13 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ClrDatagridFilter, ClrDatagridFilterInterface } from '@clr/angular';
 import { Subject } from 'rxjs';
 
-type SituacaoFilter = { property: string; value: string };
-type SituacaoOpcoes = 'A' | 'I' | 'TODOS';
-type ClrFilter = ClrDatagridFilterInterface<SituacaoFilter>;
+enum SituacaoFilter {
+  Ativos = 'A',
+  Inativos = 'I',
+  Todos = '',
+}
+
+type Retorno = { property: string; value: string };
 
 @Component({
   selector: 'app-cliente-datagrid-situacao-filter',
@@ -12,24 +16,34 @@ type ClrFilter = ClrDatagridFilterInterface<SituacaoFilter>;
   styleUrls: ['./cliente-datagrid-situacao-filter.component.css'],
 })
 export class ClienteDatagridSituacaoFilterComponent
-  implements ClrFilter, CustomDgFilter
+  implements ClrDatagridFilterInterface<Retorno>, CustomDgFilter
 {
-  @ViewChild('focus', { read: ElementRef }) input: ElementRef;
+  @ViewChild('ativos', { read: ElementRef }) ativos: ElementRef;
+  @ViewChild('inativos', { read: ElementRef }) inativos: ElementRef;
+  @ViewChild('todos', { read: ElementRef }) todos: ElementRef;
 
   changes = new Subject();
-  selected: SituacaoOpcoes = 'TODOS';
+  value = SituacaoFilter.Todos;
 
-  get state(): SituacaoFilter {
-    return { property: 'situacao', value: this.selected };
+  get state(): Retorno {
+    return { property: 'situacao', value: this.value };
+  }
+
+  get selectedElement(): ElementRef<any> {
+    return this.value === SituacaoFilter.Ativos
+      ? this.ativos
+      : this.value === SituacaoFilter.Inativos
+      ? this.inativos
+      : this.todos;
   }
 
   constructor(filterContainer: ClrDatagridFilter) {
     filterContainer.setFilter(this);
   }
 
-  focus = () => setTimeout(() => this.input.nativeElement.focus(), 0);
+  isActive = () => this.value !== SituacaoFilter.Todos;
 
-  isActive = () => this.selected !== 'TODOS';
+  accepts = (novo: Retorno) => novo.value !== this.value;
 
-  accepts = (novo: SituacaoFilter) => novo.value !== this.selected;
+  focus = () => setTimeout(() => this.selectedElement.nativeElement.focus(), 0);
 }
