085bb183e24cdebf4495a38174d8f8d60d39ead8
mrmokwa <matheus.mokwa@gmail.com>
2021-05-25 11:04:28 -0300

    remove unnecessary imports

--- src/app/core/interceptors/http-interceptor/http-interceptor.component.ts
+++ src/app/core/interceptors/http-interceptor/http-interceptor.component.ts
@@ -1,11 +1,9 @@
 import {
-  HttpEvent,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
 
 const TOKEN =
   'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIxIiwidW5pcXVlX25hbWUiOiJhZG1pbmlzdHJhZG9yIiwiZW1haWwiOiJhZG1pbmlzdHJhZG9yQHRkcy5jb20uYnIiLCJQZXNzb2FJZCI6IjAiLCJUaXBvVXN1YXJpbyI6IkEiLCJFbXByZXNhSWQiOiIxMSIsIlVybEJhc2UiOiJodHRwOi8vdGRzc2VydmVyODo4MDgwL3BhaW5lbC1kZS12ZW5kYXMvcmVzdC9wYWluZWwtZGUtdmVuZGFzLXNlcnZpY2UiLCJuYmYiOjE2MjAyNDAwMjYsImV4cCI6MTcyMDg0NDgyNiwiaWF0IjoxNjIwMjQwMDI2fQ.9RVWjB-AGmJLJ7DDkbBgE-qTNsyTK-h0yDa2fUpJMFFUF-q9fQnQIrRY9uJn6xybpGh9lTeO_AlWau3TlcDcFg';
