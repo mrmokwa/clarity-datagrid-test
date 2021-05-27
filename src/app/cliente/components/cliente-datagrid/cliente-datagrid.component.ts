e85a508635bdec1890e40ff878c8b5c940f3e838
mrmokwa <matheus.mokwa@gmail.com>
2021-05-27 11:22:15 -0300

    cancel request when filter change

--- src/app/cliente/components/cliente-datagrid/cliente-datagrid.component.ts
+++ src/app/cliente/components/cliente-datagrid/cliente-datagrid.component.ts
@@ -1,34 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ClrDatagridStateInterface } from '@clr/angular';
+import { Subject } from 'rxjs';
+import { map, switchMap, tap } from 'rxjs/operators';
 import { CustomClrDgFilter } from 'src/app/shared/components';
 import { Cliente, ClienteDetalhado, ClienteFilter } from '../../cliente.model';
 import { ClienteService } from '../../services';
+
+type State = ClrDatagridStateInterface<ClienteDetalhado>;
 
 @Component({
   selector: 'app-cliente-datagrid',
   templateUrl: './cliente-datagrid.component.html',
   styleUrls: ['./cliente-datagrid.component.css'],
 })
-export class ClienteDatagridComponent {
+export class ClienteDatagridComponent implements OnInit {
   lista: Cliente[];
   total: number;
   loading = true;
+  request = new Subject<State>();
 
   constructor(private service: ClienteService) {}
 
-  refresh(state: ClrDatagridStateInterface<ClienteDetalhado>): void {
-    this.loading = true;
-
-    this.service
-      .getAll(this.service.filters<ClienteFilter>(state))
+  ngOnInit() {
+    this.request
+      .pipe(
+        tap(() => (this.loading = true)),
+        map((state) => this.service.filters<ClienteFilter>(state)),
+        switchMap((filters) => this.service.getAll(filters))
+      )
       .subscribe(({ data, paginacao }) => {
+        this.loading = false;
         this.lista = data;
         this.total = paginacao.registros;
-        this.loading = false;
       });
   }
 
-  focus(open: boolean, ref: CustomClrDgFilter) {
-    if (open) ref.focus();
-  }
+  refresh = (state: State) => this.request.next(state);
+
+  focus = (op: boolean, ref: CustomClrDgFilter) => (op ? ref.focus() : null);
 }
--- src/app/pedvend/components/pedvend-datagrid/pedvend-datagrid.component.ts
+++ src/app/pedvend/components/pedvend-datagrid/pedvend-datagrid.component.ts
@@ -1,31 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ClrDatagridStateInterface } from '@clr/angular';
+import { Subject } from 'rxjs';
+import { map, switchMap, tap } from 'rxjs/operators';
 import { SelecaoDias } from 'src/app/shared/components';
 import { Pedido, PedidoDetalhado, PedidoFiltro } from '../../pedvend.model';
 import { PedVendService } from '../../services';
+
+type State = ClrDatagridStateInterface<PedidoDetalhado>;
 
 @Component({
   selector: 'app-pedvend-datagrid',
   templateUrl: './pedvend-datagrid.component.html',
   styleUrls: ['./pedvend-datagrid.component.css'],
 })
-export class PedvendDatagridComponent {
+export class PedvendDatagridComponent implements OnInit {
   pedidos: Pedido[];
   total: number;
   loading = true;
   diasEntrada: SelecaoDias = SelecaoDias.Mes;
+  request = new Subject<State>();
 
   constructor(private service: PedVendService) {}
 
-  refresh(state: ClrDatagridStateInterface<PedidoDetalhado>): void {
-    this.loading = true;
-
-    this.service
-      .getAll(this.service.filters<PedidoFiltro>(state))
+  ngOnInit() {
+    this.request
+      .pipe(
+        tap(() => (this.loading = true)),
+        map((state) => this.service.filters<PedidoFiltro>(state)),
+        switchMap((filter) => this.service.getAll(filter))
+      )
       .subscribe(({ data, paginacao }) => {
+        this.loading = false;
         this.pedidos = data;
         this.total = paginacao.registros;
-        this.loading = false;
       });
   }
+
+  refresh = (state: State) => this.request.next(state);
 }
